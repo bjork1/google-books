70e10dfa55a895e05f52c7dccb4500e25e75130a
Andrew Bjork <bjorkandrew1@gmail.com>
2020-05-16 11:48:35 -0500

    functional search from api

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,6 +1,21 @@
-import React, {Component} from 'react';
+import React, {Component, useState, useEffect} from 'react';
 //import queryString from 'query-string';
 import ReactDOM from 'react-dom';
+import BootstrapTable from 'react-bootstrap-table-next';
+import filterFactory, { textFilter } from 'react-bootstrap-table2-filter';
+const columns = [{
+    dataField: 'id',
+    text: 'Product ID'
+  }, {
+    dataField: 'name',
+    text: 'Product Name'
+  }, {
+    dataField: 'price',
+    text: 'Product Price',
+    filter: textFilter() // apply text filter
+  }];
+
+
 
 
 class Search extends Component{
@@ -11,32 +26,77 @@ class Search extends Component{
 //     query: '',
 //     data: [],
 // }
+
 constructor(props) {
     super(props);
+    //this.state = {value: ''};
+    //this.state = {bookData: ''};
     this.state = {
-      initialState: "Search for movies, shows, actors, etc...",
-      currentText: " "
-    }
+        results: [],
+        resultsTwo: [],
+        resultsThree: [],
+        value: []
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
-handleInputChange = () => {
-    this.setState({
-        query: this.search.value
-    })
-    this.filterArray();
-}
 
 
-      componentDidMount() {
-        fetch('https://www.googleapis.com/books/v1/volumes?q=peace+intitle:war&download=epub&key=AIzaSyDBgrZrpEGl_m_cONevbIKuqoY9_RX4pXw').then((response) =>
-       response.json()
-     )
-     .then(currentText => this.setState({currentText}))
+// state = {
+//     query: '',
+//     results: []
+// }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+    
+  }
+
+  handleSubmit(event) {
+    console.log('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+
+    this.componentDidMount()
+  }
+
+  
+componentDidMount() {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=${this.state.value}&download=epub&key=AIzaSyDBgrZrpEGl_m_cONevbIKuqoY9_RX4pXw`).then((response) =>
+    response.json()
+  )
+  .then(data => this.setState({results: data.items[0].volumeInfo.title, resultsTwo: data.items[1].volumeInfo.title, resultsThree: data.items[2].volumeInfo.title}, console.log(data.items[0].volumeInfo.title)))
       }
+      
+      handleInputChange = () => {
+        this.setState({
+          query: this.search.value
+        }, () => {
+          if (this.state.query && this.state.query.length > 1) {
+            if (this.state.query.length % 2 === 0) {
+              this.componentDidMount()
+            }
+          } 
+        })
+    }
+      
+
+
+
+
 render() {
 return(
 <div>
 
-<form>
+<form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input placeholder="Search for..." type="text" ref={input => this.search = input} value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Submit" />
+      </form>
+      {/* <Search search={search} /> */}
+{/* <form onSubmit={this.handleSubmit}>
          <label htmlFor="username">Search for Books</label>
          <input
            type="text"
@@ -44,7 +104,16 @@ return(
            onChange={this.changeText.bind(this, 'currentText')}
          />
          <button onClick = {this.changeText.bind(this, 'currentText')}>Search!</button>
-       </form>
+       </form> */}
+       <div>
+           {/* <p>Hey {this.state.value}</p>
+           <p>Hello {this.totalReactPackages}</p> */}
+           <p>{this.state.results}</p>
+           <p>{this.state.resultsTwo}</p>
+           <p>{this.state.resultsThree}</p>
+       </div>
+
+       {/* <BootstrapTable keyField='id' data={ results } columns={ columns } filter={ filterFactory() } /> */}
 
 </div>
 )
